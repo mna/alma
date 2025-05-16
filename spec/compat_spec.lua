0cb6118aaf38c0e687dac7b768fc0de9ad7c44ee
Martin Angers <martin.n.angers@gmail.com>
2025-05-16 10:50:04 -0400

    Finalize test of compat module

--- spec/compat_spec.lua
+++ spec/compat_spec.lua
@@ -1,4 +1,85 @@
+local assert = require('luassert')
 local inspect = require('inspect')
+describe('table_pack, table_unpack', function()
+  local table_pack, table_unpack
+  setup(function()
+    do
+      local _obj_0 = require('alma.compat')
+      table_pack, table_unpack = _obj_0.table_pack, _obj_0.table_unpack
+    end
+  end)
+  it('is empty for no argument', function()
+    local t = table_pack()
+    assert.are.same({
+      n = 0
+    }, t)
+    local a = table_unpack(t, 1, t.n)
+    return assert.is_nil(a)
+  end)
+  it('is as expected for one non-nil argument', function()
+    local t = table_pack(1)
+    assert.are.same({
+      n = 1,
+      [1] = 1
+    }, t)
+    local a, b = table_unpack(t, 1, t.n)
+    assert.are.equal(1, a)
+    return assert.is_nil(b)
+  end)
+  it('is as expected for two non-nil arguments', function()
+    local t = table_pack(1, 'b')
+    assert.are.same({
+      n = 2,
+      [1] = 1,
+      [2] = 'b'
+    }, t)
+    local a, b, c = table_unpack(t, 1, t.n)
+    assert.are.equal(1, a)
+    assert.are.equal('b', b)
+    return assert.is_nil(c)
+  end)
+  it('is as expected for three non-nil arguments', function()
+    local t = table_pack(1, 'b', true)
+    assert.are.same({
+      n = 3,
+      [1] = 1,
+      [2] = 'b',
+      [3] = true
+    }, t)
+    local a, b, c, d = table_unpack(t, 1, t.n)
+    assert.are.equal(1, a)
+    assert.are.equal('b', b)
+    assert.is_true(c)
+    return assert.is_nil(d)
+  end)
+  it('identifies the single value even if it is nil', function()
+    local t = table_pack(nil)
+    assert.are.same({
+      n = 1,
+      [1] = nil
+    }, t)
+    local a, b = table_unpack(t, 1, t.n)
+    assert.is_nil(a)
+    return assert.is_nil(b)
+  end)
+  return it('identifies all values even if it has nils', function()
+    local t = table_pack('a', nil, 'c', nil, nil, 'f', nil)
+    assert.are.same({
+      n = 7,
+      [1] = 'a',
+      [3] = 'c',
+      [6] = 'f'
+    }, t)
+    local a, b, c, d, e, f, g = table_unpack(t, 1, t.n)
+    assert.are.equal('a', a)
+    assert.is_nil(b)
+    assert.are.equal('c', c)
+    assert.is_nil(d)
+    assert.is_nil(e)
+    assert.are.equal('f', f)
+    return assert.is_nil(g)
+  end)
+end)
 return describe('math_type', function()
   local math_type
   setup(function()
--- spec/compat_spec.moon
+++ spec/compat_spec.moon
@@ -1,4 +1,59 @@
+assert = require 'luassert'
 inspect = require 'inspect'
+
+describe 'table_pack, table_unpack', ->
+	local table_pack, table_unpack
+	setup ->
+		{:table_pack, :table_unpack} = require 'alma.compat'
+
+	it 'is empty for no argument', ->
+		t = table_pack!
+		assert.are.same {n: 0}, t
+		a = table_unpack t, 1, t.n
+		assert.is_nil a
+
+	it 'is as expected for one non-nil argument', ->
+		t = table_pack 1
+		assert.are.same {n: 1, [1]: 1}, t
+		a, b = table_unpack t, 1, t.n
+		assert.are.equal 1, a
+		assert.is_nil b
+
+	it 'is as expected for two non-nil arguments', ->
+		t = table_pack 1, 'b'
+		assert.are.same {n: 2, [1]: 1, [2]: 'b'}, t
+		a, b, c = table_unpack t, 1, t.n
+		assert.are.equal 1, a
+		assert.are.equal 'b', b
+		assert.is_nil c
+
+	it 'is as expected for three non-nil arguments', ->
+		t = table_pack 1, 'b', true
+		assert.are.same {n: 3, [1]: 1, [2]: 'b', [3]: true}, t
+		a, b, c, d = table_unpack t, 1, t.n
+		assert.are.equal 1, a
+		assert.are.equal 'b', b
+		assert.is_true c
+		assert.is_nil d
+
+	it 'identifies the single value even if it is nil', ->
+		t = table_pack nil
+		assert.are.same {n: 1, [1]: nil}, t
+		a, b = table_unpack t, 1, t.n
+		assert.is_nil a
+		assert.is_nil b
+
+	it 'identifies all values even if it has nils', ->
+		t = table_pack 'a', nil, 'c', nil, nil, 'f', nil
+		assert.are.same {n: 7, [1]: 'a', [3]: 'c', [6]: 'f'}, t
+		a, b, c, d, e, f, g = table_unpack t, 1, t.n
+		assert.are.equal 'a', a
+		assert.is_nil b
+		assert.are.equal 'c', c
+		assert.is_nil d
+		assert.is_nil e
+		assert.are.equal 'f', f
+		assert.is_nil g
 
 describe 'math_type', ->
 	local math_type
